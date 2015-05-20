c45149e98e8b657f9779a2b3c0ee929e98e81275
Robert Person <rob@robperson.com>
2015-05-20 15:27:26 -0700

    first project

---
+++ CodingWithCat/Project1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Program
+    {
+        /*
+         *  input two numbers, separated by one space, and output the sum of the two numbers
+         *  Quit when given 0 for both numbers
+         */
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var line = Console.ReadLine();
+                var inputs = line.Split(' ');
+                var num1 = int.Parse(inputs[0]);
+                var num2 = int.Parse(inputs[1]);
+
+                if (num1 == 0 && num2 == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine(num1 + num2);
+            }
+        }
+    }
+}
